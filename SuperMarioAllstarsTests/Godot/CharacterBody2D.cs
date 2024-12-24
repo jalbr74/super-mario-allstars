7609fd64f5a4bd7fc66474cba4d08563c44d5d6d
James Albright <jalbr74@gmail.com>
2024-12-24 10:56:42 -0700

    Added unit tests

---
+++ SuperMarioAllstarsTests/Godot/CharacterBody2D.cs
@@ -0,0 +1,15 @@
+﻿namespace Godot;
+
+public class CharacterBody2D
+{
+    public virtual Vector2 Velocity
+    {
+        get => throw new NotImplementedException();
+        set=> throw new NotImplementedException();
+    }
+    
+    public virtual void _PhysicsProcess(double delta) => throw new NotImplementedException();
+    public virtual Vector2 GetGravity() => throw new NotImplementedException();
+    public void MoveAndSlide() => throw new NotImplementedException();
+    public bool IsOnFloor() => throw new NotImplementedException();
+}
---
+++ SuperMarioAllstarsTests/Objects/Characters/Mario/TestMario.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Godot;
+using Moq;
+using NUnit.Framework;
+
+namespace SuperMarioAllstarsTests;
+
+public class TestMario
+{
+    [Test]
+    public void TestOne()
+    {
+        var mario = new Mock<Mario>();
+        
+        mario.Setup(m => m.Velocity).Returns(new Vector2(1, 2));
+        mario.Setup(m => m.GetGravity()).Returns(new Vector2(42, 69));
+        
+        mario.Object.GetMessage().Should().Be("Hello, Godot! Gravity is (42, 69).");
+    }
+}
